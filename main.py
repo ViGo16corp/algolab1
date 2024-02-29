0064b5d5ca877c7815a07d6a0619affbb7fd4ec0
ViGo16 <67188772+ViGo16corp@users.noreply.github.com>
2024-02-29 23:08:44 +0200

    Adding code and tests for lab1

---
+++ main.py
@@ -0,0 +1,32 @@
+def is_subarray(nums1, nums2):
+    if not nums1:
+        return True
+
+    # Індекс для nums1
+    i = 0
+
+    # Індекс для nums2
+    j = 0
+
+    while j < len(nums2):
+        # Якщо елементи співпадають, переходимо до наступного елементу в nums1
+        if nums1[i] == nums2[j]:
+            i += 1
+            if i == len(nums1):
+                return True
+        j += 1
+
+    return False
+
+# Приклади використання:
+nums1_1 = [1, 2, 3]
+nums2_1 = [1, 2, 3, 4]
+print(is_subarray(nums1_1, nums2_1))  # True
+
+nums1_2 = [4, 2]
+nums2_2 = [1, 2, 3, 4]
+print(is_subarray(nums1_2, nums2_2))  # False
+
+nums1_3 = [1, 3, 5]
+nums2_3 = [1, 2, 3, 4, 5]
+print(is_subarray(nums1_3, nums2_3))  # True
